a40b509acf6a3b2bc9530fb26a3e7b6df5463818
Enrico Gamberini <enrico.gamberini@cern.ch>
2019-02-20 11:36:37 +0100

    add plugin system

---
+++ Modules/BoardReader.cpp
@@ -0,0 +1,42 @@
+// enrico.gamberini@cern.ch
+
+#include "BoardReader.hpp"
+
+extern "C" BoardReader *create_object()
+{
+    return new BoardReader;
+}
+
+extern "C" void destroy_object(BoardReader *object)
+{
+    delete object;
+}
+
+BoardReader::BoardReader()
+{
+    m_run = false;
+    std::cout << "BoardReader::BoardReader()" << std::endl;
+}
+
+BoardReader::~BoardReader()
+{
+    std::cout << "BoardReader::~BoardReader()" << std::endl;
+}
+
+void BoardReader::start()
+{
+    std::cout << "BoardReader::start" << std::endl;
+}
+
+void BoardReader::stop()
+{
+    std::cout << "BoardReader::stop" << std::endl;
+}
+
+void BoardReader::runner()
+{
+    while (m_run)
+    {
+        std::cout << "BoardReader::runner" << std::endl;
+    }
+}
---
+++ Modules/EventBuilder.cpp
@@ -0,0 +1,38 @@
+// enrico.gamberini@cern.ch
+
+#include "EventBuilder.hpp"
+
+extern "C" EventBuilder *create_object()
+{
+    return new EventBuilder;
+}
+
+extern "C" void destroy_object(EventBuilder *object)
+{
+    delete object;
+}
+
+EventBuilder::EventBuilder()
+{
+    std::cout << "EventBuilder::EventBuilder" << std::endl;
+}
+
+EventBuilder::~EventBuilder()
+{
+    std::cout << "EventBuilder::~EventBuilder" << std::endl;
+}
+
+void EventBuilder::start()
+{
+    std::cout << "EventBuilder::start" << std::endl;
+}
+
+void EventBuilder::stop()
+{
+    std::cout << "EventBuilder::stop" << std::endl;
+}
+
+void EventBuilder::runner()
+{
+    
+}
