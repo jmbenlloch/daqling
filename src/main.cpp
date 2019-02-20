226b16934ff7de65d043214f7c3062e347c1a46f
Roland <cbforeva@gmail.com>
2019-02-20 15:11:25 +0100

    logging in libutilities

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-//./sinks/stdout_color_sinks.h
-//#include <spdlog/spdlog.h>
+#include "utilities/logging.hpp"
 
 //using namespace spdlog;
 
 int main() {
+    
     std::cout << "Hello c++17!" << std::endl;
-    // create color multi threaded logger
-    auto console = spdlog::stdout_color_mt("console");
-    console->info("Welcome to spdlog!");
-    console->error("Some error message with arg: {}", 1);
 
-    auto err_logger = spdlog::stderr_color_mt("stderr");
-    err_logger->error("Some error message");
+    INFO("YOLO " << 12345);
+
+    // create color multi threaded logger
+    //auto console = spdlog::stdout_color_mt("console");
+    //console->info("Welcome to spdlog!");
+    //console->error("Some error message with arg: {}", 1);
+
+    //auto err_logger = spdlog::stderr_color_mt("stderr");
+    //err_logger->error("Some error message");
 
     // Formatting examples
-    console->warn("Easy padding in numbers like {:08d}", 12);
-    console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    console->info("Support for floats {:03.2f}", 1.23456);
+    //console->warn("Easy padding in numbers like {:08d}", 12);
+    //console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    //console->info("Support for floats {:03.2f}", 1.23456);
 }
 
