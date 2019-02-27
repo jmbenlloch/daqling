9b1dc43bcce9a421b7c92914e7f2bc9061538811
Enrico Gamberini <enrico.gamberini@cern.ch>
2019-02-27 17:19:42 +0100

    add options to specific testing

--- Control/daqinterface.py
+++ Control/daqinterface.py
@@ -1,3 +1,5 @@
+# enrico.gamberini@cern.ch
+
 import sys
 import zmq
 import msgpack
@@ -11,46 +13,47 @@ exe = '/home/engamber/workspace/daq/build/bin/main_core'
 with open('config.json') as f:
     data = json.load(f)
 
-print(data)
-
 arg = "complete"
 if len(sys.argv) == 1:
-    print("Available: 'remove' 'supervisor'. Going for complete service")
+    print("Available: 'remove' 'supervisor' 'jzonmq'. Going for complete service")
 else:
     arg = sys.argv[1]
 
-for p in data:
-    sd = supervisord.supervisord(p['host'], group)
+if arg == "remove" or arg == 'complete':
+    for p in data:
+        sd = supervisord.supervisord(p['host'], group)
 
-    info = sd.getAllProcessInfo()
-    for i in info:
-        print(i)
-        if i['statename'] == 'RUNNING':
-            print('Stop', sd.stopProcess(i['name']))
-            print('State', sd.getProcessState(i['name'])['statename'])
-        print('Remove', sd.removeProcessFromGroup(i['name']))
+        info = sd.getAllProcessInfo()
+        for i in info:
+            print(i)
+            if i['statename'] == 'RUNNING':
+                print('Stop', sd.stopProcess(i['name']))
+                print('State', sd.getProcessState(i['name'])['statename'])
+            print('Remove', sd.removeProcessFromGroup(i['name']))
 
-if arg == "remove":
-    exit(0)
+if arg == 'supervisor' or arg == 'complete':
+    for p in data:
+        sd = supervisord.supervisord(p['host'], group)
 
-for p in data:
-    sd = supervisord.supervisord(p['host'], group)
-
-    print("Add", sd.addProgramToGroup(p['name'], exe+" "+str(p['port']), dir) )
+        print("Add", sd.addProgramToGroup(p['name'], exe+" "+str(p['port']), dir) )
 
 
-if arg == "supervisor":
-    exit(0)
-
-for p in data:
+if arg == 'jzonmq' or arg == 'complete':
     context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    socket.connect("tcp://"+p['host']+":"+str(p['port']))
-    p['command']='configure'
-    s = json.dumps(p)
-    print(s)
-    socket.send_string(s)
-    print("sent")
+    for p in data:
+        socket = context.socket(zmq.REQ)
+        socket.RCVTIMEO = 3000
+        socket.connect("tcp://"+p['host']+":"+str(p['port']))
+        p['command']='configure'
+        s = json.dumps(p)
+        print(s)
+        socket.send_string(s)
+        print("sent")
 
-    reply = socket.recv()
-    print(reply)
+        try:
+            reply = socket.recv()
+            print(reply)
+        except:
+            print("Timeout occurred")
+
+
