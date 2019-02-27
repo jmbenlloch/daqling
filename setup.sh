433083b30dd09a4c2b287864eb2237d89606d335
MBoretto <marco.bore@gmail.com>
2019-02-27 15:27:07 +0100

    Add cvmfs role

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+
+export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/gcc/6.2.0/x86_64-centos7/lib64/:$LD_LIBRARY_PATH
+
+
+#/cvmfs/sft.cern.ch/lcg/releases/gcc/6.2.0/x86_64-centos7/bin/g++ main.cpp
