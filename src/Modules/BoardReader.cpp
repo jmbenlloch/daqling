670d2b65b6a0b0a4dfcf2421c78dc9efaca2b470
Enrico Gamberini <enrico.gamberini@cern.ch>
2019-03-13 11:12:26 +0100

    add random size data generator in board reader

--- src/Modules/BoardReader.cpp
+++ src/Modules/BoardReader.cpp
@@ -7,10 +7,25 @@
 #include <thread>
 #include <chrono>
 
+// #include <stdlib.h>
+
 #define __METHOD_NAME__ daq::utilities::methodName(__PRETTY_FUNCTION__)
 #define __CLASS_NAME__ daq::utilities::className(__PRETTY_FUNCTION__)
 
 using namespace std::chrono_literals;
+using namespace std::chrono;
+
+struct header_t {
+    uint32_t payload_size;
+    uint32_t source_id;
+    uint32_t seq_number;
+    uint64_t timestamp;
+};
+
+struct data_t {
+    header_t header;
+    char payload[];
+};
 
 extern "C" BoardReader *create_object(std::string name, int num)
 {
@@ -57,10 +72,28 @@ void BoardReader::stop()
 
 void BoardReader::runner()
 {
+    const unsigned source_id = 1;
+    unsigned sequence_number = 0;
+    microseconds timestamp;
+
     while (m_run)
     {
         INFO(__METHOD_NAME__ << " Running...");
-        std::this_thread::sleep_for(2s);
+        timestamp = duration_cast<microseconds>(system_clock::now().time_since_epoch());
+        const unsigned payload_size = rand() % 1000 + 1;
+        INFO(__METHOD_NAME__ << " sequence number " << sequence_number
+            << "  >>  timestamp " << timestamp.count() << "  >>  payload size " << payload_size );
+
+        data_t * data = (data_t *) malloc(sizeof(data_t) + sizeof(char)*payload_size);
+        data->header.payload_size = payload_size;
+        data->header.seq_number = sequence_number;
+        data->header.source_id = source_id;
+        data->header.timestamp = timestamp.count();
+        memset(data->payload, 'F', payload_size);
+        // ready to be sent to EB
+
+        sequence_number++;
+        std::this_thread::sleep_for(500ms);
     }
     INFO(__METHOD_NAME__ << " Runner stopped");
 }
