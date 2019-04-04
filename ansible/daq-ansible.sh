a007769650b5221167f1f06683bb4e473050c0db
Roland Sipos <rsipos@cern.ch>
2019-04-04 15:01:19 +0200

    Reorg. + ansible cfg

--- ansible/daq-ansible.sh
+++ ansible/daq-ansible.sh
@@ -3,7 +3,7 @@
 # Set ansible environment variables to run from local directory instead
 # of /etc/ansible.
 # 
-ansible_home=`pwd`
+ansible_home=`pwd`/playbooks
 export ANSIBLE_CONFIG=$ansible_home/ansible.cfg
 export ANSIBLE_INVENTORY=$ansible_home/hosts
 export ANSIBLE_LIBRARY=$ansible_home
