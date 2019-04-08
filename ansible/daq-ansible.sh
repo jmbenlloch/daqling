28820410e4743dc904c01287c5d526ae6ad2a300
Enrico Gamberini <enrico.gamberini@cern.ch>
2019-04-08 17:47:02 +0200

    setup cassandra driver via ansible and cmake

--- ansible/daq-ansible.sh
+++ ansible/daq-ansible.sh
@@ -6,4 +6,4 @@
 ansible_home=`pwd`/playbooks
 export ANSIBLE_CONFIG=$ansible_home/ansible.cfg
 export ANSIBLE_INVENTORY=$ansible_home/hosts
-export ANSIBLE_LIBRARY=$ansible_home
+export ANSIBLE_LIBRARY=$ansible_home/library
--- cmake/setup-openhpc.sh
+++ cmake/setup-openhpc.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=/opt/ohpc/pub/compiler/gcc/7.3.0/lib64/:$LD_LIBRARY_PATH
 #gcc_home=/cvmfs/sft.cern.ch/lcg/releases/gcc/7.3.0-cb1ee/x86_64-centos7
 
 #export FC=`which gfortran`
-export CC='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/c++'
+export CC='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/gcc'
 export CXX='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/g++'
 
 #export COMPILER_PATH=${gcc_home}
