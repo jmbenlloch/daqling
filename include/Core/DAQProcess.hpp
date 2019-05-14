82c3dd44df7b3c8fd99753c416d0fdbff0f570cc
Enrico Gamberini <enrico.gamberini@cern.ch>
2019-05-14 13:45:02 +0000

    copyright'd all (CERN) source code -moved COPYING to LICENSE

--- include/Core/DAQProcess.hpp
+++ include/Core/DAQProcess.hpp
@@ -1,5 +1,27 @@
-// enrico.gamberini@cern.ch
+/**
+ * Copyright (C) 2019 CERN
+ * 
+ * DAQling is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * DAQling is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DAQling. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/**
+ * @file DAQProcess.hpp
+ * @author Enrico.Gamberini@cern.ch
+ * @brief Base class for Modules loaded via the PluginManager
+ * @date 2019-02-20
+ */
+ 
 #ifndef DAQPROCESS_HPP_
 #define DAQPROCESS_HPP_
 
