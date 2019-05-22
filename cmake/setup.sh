5c40a31e7599f5703b73ca889d60b21eadb554d7
Enrico Gamberini <enrico.gamberini@cern.ch>
2019-05-22 10:53:32 +0200

    remove settings file and handle configuration via environment

---
+++ cmake/setup.sh
@@ -0,0 +1,20 @@
+export DAQ_CONFIG_DIR=$PWD/scripts/Control/
+export DAQ_BUILD_DIR=$PWD/build/
+alias daqinterface='python3 $PWD/scripts/Control/daqinterface.py'
+
+echo "Custom compiler, installed by Ansible from OHPC."
+
+export LD_LIBRARY_PATH=/opt/ohpc/pub/compiler/gcc/8.3.0/lib64/:/usr/local/lib64/:$LD_LIBRARY_PATH
+export PATH=/opt/ohpc/pub/compiler/gcc/8.3.0/bin:$PATH
+
+# Export package specific environmental variables
+
+export CC='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/gcc'
+export CXX='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/g++'
+
+export TBB_VERSION=2019.0
+export TBB_ROOT_DIR=/opt/tbb-2019_U5
+
+export BOOST_VERSION=1.70
+export BOOST_ROOT_DIR=/opt/boost
+
