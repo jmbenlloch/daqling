d3e6383dbba2a731acafe3d578cb11bd6939bf11
Viktor Vilhelm Sonesten <viktor.vilhelm.sonesten@cern.ch>
2019-07-29 15:04:32 +0200

    modules/FileDataLogger: reimplement an intermediate buffer
    Depending on the payload size, it is faster to buffer the data until a
    configured length before flushing instead of writing the data
    payload-per-payload.
    
    See
    <https://stackoverflow.com/questions/12997131/stdfstream-buffering-vs-manual-buffering-why-10x-gain-with-manual-buffering>

--- src/Modules/FileDataLogger/FileDataLoggerModule.hpp
+++ src/Modules/FileDataLogger/FileDataLoggerModule.hpp
@@ -85,7 +85,7 @@ class FileDataLoggerModule : public daqling::core::DAQProcess, public daqling::c
   std::atomic<bool> m_stopWriters;
 
   // Internals
-  void flusher(PayloadQueue &pq, FileGenerator &&fg, const uint64_t chid) const;
+  void flusher(const uint64_t chid, PayloadQueue &pq, const long max_buffer_size, FileGenerator &&fg) const;
   std::map<uint64_t, Context> m_channelContexts;
   mutable std::atomic<int> m_bytes_sent;
   std::thread m_monitor_thread;
