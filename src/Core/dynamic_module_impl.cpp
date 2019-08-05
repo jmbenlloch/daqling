9ead511365d031cef685ef3c7988380b56ddc553
Viktor Vilhelm Sonesten <viktor.vilhelm.sonesten@cern.ch>
2019-08-05 16:55:00 +0200

    core: correctly delete module inststance again

--- src/Core/dynamic_module_impl.cpp
+++ src/Core/dynamic_module_impl.cpp
@@ -2,20 +2,24 @@
     #error "This header should only be automatically included during the build with CMake"
 #endif
 
-#include "Utils/Logging.hpp"
-
 #include DAQLING_MODULE_HEADER
 
 namespace daqling::core {
     extern "C" {
         // declare to satisfy -Werror=missing-declarations
-        DAQProcess* daqling_module_generator();
+        DAQProcess* daqling_module_create();
+        void daqling_module_delete(DAQProcess*);
 
         // and then define
-        DAQProcess* daqling_module_generator()
+        DAQProcess* daqling_module_create()
         {
             auto module = new DAQLING_MODULE_NAME();
             return static_cast<DAQProcess*>(module);
+        }
+
+        void daqling_module_delete(DAQProcess *module)
+        {
+            delete static_cast<DAQLING_MODULE_NAME*>(module);
         }
     }
 }
