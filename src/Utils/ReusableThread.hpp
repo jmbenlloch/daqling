604931162ca41d2eed854c0dfc9762f5a89a7bab
Viktor Vilhelm Sonesten <viktor.vilhelm.sonesten@cern.ch>
2019-08-09 12:20:50 +0200

    Utils/ReusableThread: improve variable names

--- src/Utils/ReusableThread.hpp
+++ src/Utils/ReusableThread.hpp
@@ -36,12 +36,11 @@
 namespace daqling {
 namespace utilities {
 
-
 class ReusableThread
 {
 public:
   ReusableThread(unsigned int threadID)
-    : m_thread_id(threadID), m_thread_pause(true), m_thread_quit(false), m_function_ready(false),
+    : m_thread_id(threadID), m_task_executed(true), m_task_assigned(false), m_thread_quit(false),
       m_thread(&ReusableThread::thread_worker, this)
   { }
 
@@ -56,13 +55,13 @@ public:
 
   unsigned int get_thread_id() const { return m_thread_id; }
 
-  bool get_readiness() const { return m_thread_pause; }
+  bool get_readiness() const { return m_task_executed; }
 
-  bool set_work(const std::function<void()>& work_func)
+  bool set_work(const std::function<void()>& task)
   {
-    if (!m_function_ready && m_thread_pause.exchange(false)) {
-      m_work_func = work_func;
-      m_function_ready = true;
+    if (!m_task_assigned && m_task_executed.exchange(false)) {
+      m_task = task;
+      m_task_assigned = true;
       m_cv.notify_all();
       return true;
     }
@@ -71,10 +70,10 @@ public:
 
 private:
   unsigned int m_thread_id;
-  std::atomic<bool> m_thread_pause;
+  std::atomic<bool> m_task_executed;
+  std::atomic<bool> m_task_assigned;
   std::atomic<bool> m_thread_quit;
-  std::atomic<bool> m_function_ready;
-  std::function<void()> m_work_func;
+  std::function<void()> m_task;
 
   std::mutex m_mtx;
   std::condition_variable m_cv;
@@ -82,12 +81,13 @@ private:
 
   void thread_worker()
   {
-    std::unique_lock<std::mutex> lock(m_mtx); // <- lock the mutex
+    std::unique_lock<std::mutex> lock(m_mtx);
+
     while (!m_thread_quit){
-      if (!m_thread_pause && m_function_ready){
-        m_work_func();
-        m_thread_pause = true;
-        m_function_ready = false;
+      if (!m_task_executed && m_task_assigned){
+        m_task();
+        m_task_executed = true;
+        m_task_assigned = false;
       } else {
         m_cv.wait(lock);
       }
