8e479ad277345637aeed579ab79bad5abb8c17c5
Roland Sipos <roland.sipos@cern.ch>
2020-05-19 07:29:20 +0000

    Crossbuild features * add Ansible roles for installation of Linaro toolchain * add setup scripts for cross-compilation of different architectures * build with CPR made optional

---
+++ ansible/roles/toolchain/files/setup.sh
@@ -0,0 +1,10 @@
+echo "Custom toolchain setup, deployed by Ansible."
+
+# Export toolchain libraries and setup path
+#export LD_LIBRARY_PATH=/opt/ohpc/pub/compiler/gcc/8.3.0/lib64/:/usr/local/lib64/:$LD_LIBRARY_PATH
+#export PATH=/opt/ohpc/pub/compiler/gcc/8.3.0/bin:$PATH
+
+# Setup toolchain compilers
+#export CC='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/gcc'
+#export CXX='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/g++'
+
